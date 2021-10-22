76d72105130da5250876e3f414fef3ef37759502
unknown <am9642342@gmail.com>
2021-10-22 22:25:54 +0200

    adding smooth and styling

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # Landing Page Project
 
-## Table of Contents
+## Language Used
+HTML
+CSS
+javaScript
 
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+## Description
+Static web site is made from html css javascript this project is responsie web page
--- js/app.js
+++ js/app.js
@@ -1,62 +1,43 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+var Sections = Array.from(document.querySelectorAll('section'));
+var ListItem = document.getElementById("navbar__list");
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+// creat a lits item in Html file
+function CreateListItem() {
+    for (section of Sections){
+        NameItem = section.getAttribute("data-nav");
+        LinkSection = section.getAttribute("id");
+        fItem = document.createElement("li");
+        fItem.innerHTML = `<a class='menu__link' href='#${LinkSection}' data-nav='${LinkSection}'>${NameItem}</a>`; // creating link in list item
+        ListItem.appendChild(fItem);
+    }
+}
+// this function is adding a style states to the navbar items
+window.onscroll = function() {
+	document.querySelectorAll("section").forEach(function(activeItem) {
+  var activeLink = ListItem.querySelector(`[data-nav=${activeItem.id}]`);
+  // geting and check the area of the section
+	if(activeItem.getBoundingClientRect().top >= -400 && activeItem.getBoundingClientRect().top <= 150){
 
-/**
- * Define Global Variables
- * 
-*/
+    activeItem.classList.add("your-active-class");
+    activeLink.classList.add("active-link");
 
+    } else {
+      activeItem.classList.remove("your-active-class");
+      activeLink.classList.remove("active-link");
+    }
+	});
+}
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+// this code make the page smooth
+ListItem.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.dataset.nav) {
+    document
+      .getElementById(`${event.target.dataset.nav}`)
+      .scrollIntoView({ behavior: "smooth" });
+    setTimeout(() => {
+      location.hash = `${event.target.dataset.nav}`;
+    }, 200);
+  }
+});
+CreateListItem();
